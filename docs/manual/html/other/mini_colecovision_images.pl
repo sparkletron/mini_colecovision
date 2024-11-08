65a979a0f7c0677c3bd648f307ca5f038765bc32
Jay Convertino <electrobs@gmail.com>
2024-11-08 09:09:47 -0500

    fixed image and pdfs?

--- docs/manual/html/other/mini_colecovision_images.pl
+++ docs/manual/html/other/mini_colecovision_images.pl
@@ -7,10 +7,10 @@ $cached_env_img{$key} = q|<IMG
  STYLE="height: 0.46ex; vertical-align: -0.12ex; " SRC="|."$dir".q|mini_colecovision_img2.svg"
  ALT="$ $">|; 
 
-$key = q/includegraphics[width=0.90textwidthheight=textheight,keepaspectratio]{imgslashSPARKLETRON.png};AAT/;
+$key = q/includegraphics[width=0.90textwidth,height=textheight,keepaspectratio]{imgslashSPARKLETRON.png};AAT/;
 $cached_env_img{$key} = q|<IMG
- STYLE="height: 6.75ex; vertical-align: -0.57ex; " SRC="|."$dir".q|mini_colecovision_img1.svg"
- ALT="\includegraphics[width=0.90\textwidth height=\textheight,keepaspectratio]{img/SPARKLETRON.png}">|; 
+ STYLE="height: 6.30ex; vertical-align: -0.12ex; " SRC="|."$dir".q|mini_colecovision_img1.svg"
+ ALT="\includegraphics[width=0.90\textwidth,height=\textheight,keepaspectratio]{img/SPARKLETRON.png}">|; 
 
 1;
 
